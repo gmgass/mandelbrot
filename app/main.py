582b27b44023b7ea3e946aa1c5d3b51b6e47638f
Gustavo Gass <gmgass@inf.ufpel.edu.br>
2026-07-16 20:30:47 -0300

    feat: adiciona ponte de comunicação python-rust

--- app/main.py
+++ app/main.py
@@ -10,3 +10,9 @@ try:
 except ImportError as e:
     print("Erro\n")
     sys.exit(1)
+
+print("teste de ponte do Python para o Rust...\n")
+return_message = rust_motor.integration_test(-2.0, 1.0, 150)
+
+print("\nRESPOSTA DO RUST:")
+print(return_message)
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,18 @@
-    use pyo3::prelude::*;
+use pyo3::prelude::*;
 
-    #[pymodule]
-    fn rust_motor(m: &Bound<'_, PyModule>) -> PyResult<()> {
-        Ok(())
-    }
+#[pyfunction]
+fn integration_test(xmin: f64, xmax: f64, max_iter: u32) -> PyResult<String> {
+    let _plane_width = xmax - xmin;
+    let _xmin = xmin;
+    let _xmax = xmax;
+    let _max_iter = max_iter;
+    
+    let response = "Ponte funcionando.\n".to_string();
+    Ok(response)
+}
+
+#[pymodule]
+fn rust_motor(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(integration_test, m)?)?; // Registra a função traduzida dentro do módulo Python
+    Ok(())
+}
