3873370e225e7c75d74b185d56754300c570c25f
Gustavo Gass <gmgass@inf.ufpel.edu.br>
2026-07-16 21:02:02 -0300

    feat: implementa interface com pygame e renderiza o fractal

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import pygame
 
 # Adiciona o caminho do .pyd/so
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../target/release')))
@@ -8,18 +9,32 @@ try:
     import rust_motor
     print("Sucesso\n")
 except ImportError as e:
-    print("Erro\n")
+    print("\n[ERRO] módulo motor_rust não encontrado.")
     sys.exit(1)
 
-# Parâmetros de resolução para o teste
+# config. inicial da janela de interface
 WIDTH, HEIGHT = 800, 600
-MAX_ITER = 100
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
-# Executa o cálculo no Rust
-raw_bytes = rust_motor.calculate_mandelbrot(
-    WIDTH, HEIGHT, -2.0, 1.0, -1.2, 1.2, MAX_ITER
-)
+# limites iniciais de Mandelbrot (valores sugeridos pelo copilot)
+xmin, xmax = -2.0, 1.0
+ymin, ymax = -1.2, 1.2
+max_iter = 120
 
-print("\nRESULTADO:")
-print(f"Vetor com {len(raw_bytes)} bytes do Rust.")
-print(f"Tamanho esperado de bytes: {WIDTH * HEIGHT * 3}")
+def render_mandelbrot():
+    raw_rgb_bytes = rust_motor.calculate_mandelbrot(WIDTH, HEIGHT, xmin, xmax, ymin, ymax, max_iter)
+    return pygame.image.frombuffer(bytes(raw_rgb_bytes), (WIDTH, HEIGHT), 'RGB')
+
+fractal_image = render_mandelbrot()
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.blit(fractal_image, (0, 0))
+    pygame.display.flip()
+
+pygame.quit()
